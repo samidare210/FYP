0654b53da35e8dfa4894e5b71422f2f08e726915
unknown <vrad@qq.com>
2023-01-19 16:26:54 +0800

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,19 @@ import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MuiDrawer from '@mui/material/Drawer';
 import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import CssBaseline from '@mui/material/CssBaseline';
-import Typography from '@mui/material/Typography';
-import Divider from '@mui/material/Divider';
-import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import InboxIcon from '@mui/icons-material/MoveToInbox';
-import { Button, Grid, Container, Slider } from '@mui/material'
-import { Joystick } from 'react-joystick-component';
 import ControlCameraIcon from '@mui/icons-material/ControlCamera';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Stack from '@mui/material/Stack';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import BatterySaverIcon from '@mui/icons-material/BatterySaver';
+
+import { ListItemIcon, ListItemButton, ListItem }from '@mui/material/';
+import { List, Toolbar, IconButton, Divider, Typography, CssBaseline, Button, Grid, Container, Slider, ButtonGroup, Card, CardContent, Stack, Paper, ListItemText } from '@mui/material'
+import { Joystick } from 'react-joystick-component';
+
 
 /*
   Note that the frontend is running at the port 3000
@@ -102,36 +91,44 @@ const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' 
   }),
 );
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
+
 const cardButton = (
   <React.Fragment>
     <CardContent>
-        <div className='btn-container'>
-        <Button variant='contained' 
+      <div className='btn-container'>
+        <Button variant='contained'
         // onMouseDown={ sendMsgStand }
         >STAND UP</Button>
         <Button variant='contained'
         // onMouseDown={  }
         >STAND DOWN</Button>
-        <Button variant='contained' 
+        <Button variant='contained'
         // onMouseDown={ sendMsgCrouch }
         >CROUCH DOWN</Button>
         <Button variant='contained'
-          // onMouseDown={ e => onMouseDown('move_forward') } 
-          // onMouseUp={ onMouseUp }
+        // onMouseDown={ e => onMouseDown('move_forward') } 
+        // onMouseUp={ onMouseUp }
         >MOVE FORWARD</Button>
         <Button variant='contained'
-          // onMouseDown={ e => onMouseDown('move_backward') } 
-          // onMouseUp= { onMouseUp }
+        // onMouseDown={ e => onMouseDown('move_backward') } 
+        // onMouseUp= { onMouseUp }
         >MOVE BACKWORD</Button>
         <Button variant='contained'
-          // onMouseDown={ e => onMouseDown('turn_left') } 
-          // onMouseUp={ onMouseUp}
+        // onMouseDown={ e => onMouseDown('turn_left') } 
+        // onMouseUp={ onMouseUp}
         >TURN LEFT</Button>
-        <Button variant='contained' 
-          // onMouseDown={ e => onMouseDown('turn_right') } 
-          // onMouseUp={ onMouseUp }
+        <Button variant='contained'
+        // onMouseDown={ e => onMouseDown('turn_right') } 
+        // onMouseUp={ onMouseUp }
         >TURN RIGHT</Button>
-            <DrawerHeader />
+        <DrawerHeader />
       </div>
     </CardContent>
   </React.Fragment>
@@ -139,49 +136,45 @@ const cardButton = (
 
 const JoystickBarLeft = (
   <React.Fragment>
-    <CardContent>
-    <div className='joystick-container'>
-      <Joystick>
-        size={120}
-        stickSize={60}
-        sticky={false}
-        baseColor="rgba(0, 0, 0, 0.5)"
-        stickColor="rgba(0, 0, 0, 0.7)"
-      </Joystick>
-      </div>
-    </CardContent>
+  <div className='joystick-container'>
+    <Joystick>
+      size={120}
+      stickSize={60}
+      sticky={false}
+      baseColor="rgba(0, 0, 0, 0.5)"
+      stickColor="rgba(0, 0, 0, 0.7)"
+    </Joystick>
+  </div>
   </React.Fragment>
 );
 
 const JoystickBarRight = (
   <React.Fragment>
-    <CardContent>
-      <div className='btn-container'>
-      <Joystick>
-        size={120}
-        stickSize={60}
-        sticky={false}
-        baseColor="rgba(0, 0, 0, 0.5)"
-        stickColor="rgba(0, 0, 0, 0.7)"
-      </Joystick>
-      </div>
-    </CardContent>
-  </React.Fragment>
+  <div className='btn-container'>
+    <Joystick>
+      size={120}
+      stickSize={60}
+      sticky={false}
+      baseColor="rgba(0, 0, 0, 0.5)"
+      stickColor="rgba(0, 0, 0, 0.7)"
+    </Joystick>
+  </div>
+</React.Fragment>
 );
 
 const cardModeSelect = (
   <React.Fragment>
     <CardContent>
-    <div className='joystick-container'>
-      <ToggleButton value="left" key="left">
-SPEED MODE
-      </ToggleButton>
-      <ToggleButton value="center" key="center"> 
-POSITION MODE
-      </ToggleButton>
-      <ToggleButton value="right" key="right">
-PITCH MODE
-      </ToggleButton>
+      <div className='joystick-container'>
+        <Button value="left" key="left">
+          SPEED MODE
+        </Button>
+        <Button value="center" key="center">
+          POSITION MODE
+        </Button>
+        <Button value="right" key="right">
+          PITCH MODE
+        </Button>
       </div>
     </CardContent>
   </React.Fragment>
@@ -214,88 +207,93 @@ export default function App() {
 
   return (
     <>
-      <CssBaseline />
-      <AppBar position="fixed" open={open}>
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            sx={{
-              marginRight: 2.5,
-              ...(open && { display: 'none' }),
-            }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" noWrap component="div">
-            Robot Control Dashboard
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer variant="permanent" open={open}>
-        <DrawerHeader>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {['Home', 'Message Log', 'Robot Status', 'Remote Control'].map((text, index) => (
-            <ListItem key={text} disablePadding sx={{ display: 'block' }}>
-              <ListItemButton
-                sx={{
-                  minHeight: 48,
-                  justifyContent: open ? 'initial' : 'center',
-                  px: 2,
-                }}
-              >
-                <ListItemIcon>
-{/* First line icon is HomeIcon,second line icon is InboxIcon,third line icon is SettingsIcon and the forth icon is ControlCameraIcon */}
-                  {index % 4 === 0 ? <HomeIcon /> : index % 4 === 1 ? <InboxIcon /> : index % 4 === 2 ? <SettingsIcon /> : <ControlCameraIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <DrawerHeader />
-      </Drawer>
+        <CssBaseline />
+        <AppBar position="fixed" open={open}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              onClick={handleDrawerOpen}
+              edge="start"
+              sx={{
+                marginRight: 2.5,
+                ...(open && { display: 'none' }),
+              }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap component="div">
+              Robot Control Dashboard
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Drawer variant="permanent" open={open}>
+          <DrawerHeader>
+            <IconButton onClick={handleDrawerClose}>
+              {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+            </IconButton>
+          </DrawerHeader>
+          <Divider />
+          <List>
+            {['Home', 'Message Log', 'Robot Status', 'Remote Control'].map((text, index) => (
+              <ListItem key={text} disablePadding sx={{ display: 'block' }}>
+                <ListItemButton
+                  sx={{
+                    minHeight: 48,
+                    justifyContent: open ? 'initial' : 'center',
+                    px: 2,
+                  }}
+                >
+                  <ListItemIcon>
+                    {/* First line icon is HomeIcon,second line icon is InboxIcon,third line icon is SettingsIcon and the forth icon is ControlCameraIcon */}
+                    {index % 4 === 0 ? <HomeIcon /> : index % 4 === 1 ? <InboxIcon /> : index % 4 === 2 ? <SettingsIcon /> : <ControlCameraIcon />}
+                  </ListItemIcon>
+                  <ListItemText primary={text} sx={{ opacity: open ? 1 : 0 }} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+          <Divider />
+          <DrawerHeader />
+        </Drawer>
 
-
-      <Box component="main" sx={{ flexGrow: 1, p: 14 }}>
-        <Card variant="outlined">//using camera</Card>
+        {/* <Box component="main" sx={{ display: 'flex' }}>
+          <Card variant="outlined">  </Card>
+        </Box> */}
+      <Box component="main" sx={{ flexGrow: 1, p: 11 }}>
+        <Card variant='outlined'>
+          <Stack direction="row" spacing={2} alignItems="center" justifyContent="flex-end">
+            <Item><BatterySaverIcon sx={{ mr: 0.5 }} fontSize="inherit" />Battery Percentage: {} % </Item>
+          </Stack>
+        </Card>
       </Box>
-      <Box component="main" sx={{ flexGrow: 3, p: 25 }}>
-        <Grid container spacing={6}>
-          <Grid Card xs>
-            <Card variant="outlined">
-              {JoystickBarLeft}
-            </Card>
+      
+        <Box component="main" sx={{ flexGrow: 3, p: 25 }}>
+          <Grid container spacing={6}>
+            <Grid Card xs>
+              <Card variant="outlined">
+                {JoystickBarLeft}
+              </Card>
+            </Grid>
+            <Grid Card xs >
+              <Card variant="outlined">
+                <ButtonGroup size="large" {...control} aria-label="Large sizes">
+                  {cardModeSelect}
+                </ButtonGroup>
+              </Card>
+            </Grid>
+            <Grid Card xs>
+              <Card variant="outlined">
+                {JoystickBarRight}
+              </Card>
+            </Grid>
           </Grid>
-          <Grid Card xs={3.5}>
-            <Card variant="outlined">
-            <Stack spacing={3} alignItems="baseline" >
-              <ToggleButtonGroup size="large" {...control} aria-label="Large sizes">
-        {cardModeSelect}
-      </ToggleButtonGroup>
-      </Stack>
-            </Card>
-          </Grid>
-          <Grid Card xs>
-            <Card variant="outlined">
-              {JoystickBarRight}
-            </Card>
-          </Grid>
-        </Grid>
-        <Box component="main" sx={{ flexGrow: 1, p: 6 }}>
-          <Card variant="outlined">{cardButton}</Card>
+          <Box component="main" sx={{ flexGrow: 1, p: 6 }}>
+            <Card variant="outlined">{cardButton}</Card>
+          </Box>
         </Box>
-      </Box>   
     </>
-     )
+  )
   
   ////////
   ///////
