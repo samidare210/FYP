0a9cd4347896aefeb451746f7198e9597ed84865
unknown <vrad@qq.com>
2023-01-24 14:39:02 +0800

    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
 import App from './App';
+// import SpeedLog from './SpeedLog';
+// import Open from './Open';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+    // <Router>
+    //     <Routes>
+    //         <Route path="/" element={<Control />} />
+    //         <Route path="/speedlog" element={<SpeedLog />} />
+    //         <Route path="/open" element={<Open />} />
+    //     </Routes>
+    // </Router>,
     <App />
 );
